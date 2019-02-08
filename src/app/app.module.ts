bd71c9f0c34b70ae6cc08a50e55e8bce8fdb2b90
miguel <mmeraz@netasystems.net>
2019-02-07 18:25:18 -0600

    commit prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// Modulos
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    DetalleComponent,
+    EstadisticaComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
